30550fe8e0a341e4ef594cb451b439bc44ba38b5
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-06 02:46:19 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -46,8 +46,24 @@ biological_life = {
   :label => "Kingdom",
   :sub_category => {
     :label => "Phylum",
-    :sub_category => "Class"}
-
+    :sub_category => {,
+      :label => "Class",
+      :sub_category => {
+        :label => "Order"
+        :sub_category => {
+          :label => "Family",
+          :sub_category => {
+            :label => "Genus",
+            :sub_category => {
+              :label => "Species"
+            }
+              
+          }
+        }
+      }
+          
+    }
+  }
 }
 
 end
